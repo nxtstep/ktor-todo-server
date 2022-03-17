37a1dedee2629d49ca1e1ddb1c56a3e29bf720b7
Philip Crockett <philip.crockett@thermondo.de>
2022-03-17 16:16:43 +0100

    authenticatedGet and some reorg

--- todo-server/auth-service/src/main/kotlin/io/supersimple/auth/api/error/AuthenticationException.kt
+++ todo-server/auth-service/src/main/kotlin/io/supersimple/auth/api/error/AuthenticationException.kt
@@ -1,6 +1,7 @@
-package io.supersimple.common.api.error
+package io.supersimple.auth.api.error
 
 import io.ktor.http.HttpStatusCode
+import io.supersimple.common.api.error.HttpError
 
 public class AuthenticationException(description: String? = null) :
     HttpError(HttpStatusCode.Unauthorized, description ?: "Unauthorized")
--- todo-server/auth-service/src/main/kotlin/io/supersimple/auth/util/ApplicationCallAuthenticatedUser.kt
+++
@@ -1,7 +0,0 @@
-package io.supersimple.auth.util
-
-import io.ktor.application.ApplicationCall
-import io.ktor.auth.principal
-import io.supersimple.auth.model.User
-
-val ApplicationCall.authenticatedUser: User? get() = principal() as? User
